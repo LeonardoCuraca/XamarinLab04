ca80bd011743471872c0e9c5b1a1f47abc305c72
LeonardoCuraca <leonardo.curaca@tecsup.edu.pe>
2020-04-21 17:36:28 -0500

    first commit

---
+++ Lab4/Lab4/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab4
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new NavigationPage(new Page1());
+            //MainPage = new TabbedPage();
+            //MainPage = new CarouselPage1();
+            MainPage = new NavigationPage(new Page3());
+            //MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
